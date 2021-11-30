c333e4fa34e52653d7963945c4f7ff9c6f269814
dmik-id <dmitriev.i@phystech.edu>
2021-11-30 18:57:04 +0300

    Update file.controller.ts

--- server/src/file/file.controller.ts
+++ server/src/file/file.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+
 
 @Controller('file')
-export class FileController {}
+export class FileController {
+
+    @Post('multiple')
+    @UseInterceptors(
+      FilesInterceptor('image', 20, {
+        storage: diskStorage({
+          destination: './files',
+          filename: 'test'
+        }),
+        
+      }),
+    )
+    async uploadMultipleFiles(@UploadedFiles() files) {
+      const response = [];
+      files.forEach(file => {
+        const fileReponse = {
+          originalname: file.originalname,
+          filename: file.filename,
+        };
+        response.push(fileReponse);
+      });
+      return response;
+    }
+
+}
+function diskStorage(arg0: { destination: string; filename: any; }): any {
+    throw new Error('Function not implemented.');
+}
+
